289a5e27297f1f77742656f18ecbf30ae1924a41
Bikram Dhoju <bikram.dhoju@olenepal>
2018-06-13 11:30:54 +0545

    deploy version fix

--- deploy.sh
+++ deploy.sh
@@ -175,8 +175,8 @@ release_is_number() {
 
 get_deploy_version(){
 
-DATE= `date '+%Y%m%d%H%M%S'`
-sed -i 's/^v1.0.*/v1.0-$(DATE)/g' src/templates/static_pages/about.html
+DATE=`date '+%Y%m%d%H%M%S'`
+sed -i 's/^v1.0.*/v1.0-$DATE/g' src/templates/static_pages/about.html
 
 }
 
