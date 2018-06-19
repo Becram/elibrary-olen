10459a821319edba9f7532161c8bcd2c682a8c5d
bikram.dhoju <bikram.dhoju@olenepal.org>
2018-06-19 14:03:32 +0545

    deploy edit

--- deploy.sh
+++ deploy.sh
@@ -96,7 +96,7 @@ process() {
                   # notify "Backing up"
                   # docker_backup_local "dev"
                   notify "Removing containers"
-                  docker_remove "prod"
+                  docker_remove "dev"
 
                   # notify "Deletelting UNTAGGED images"
                   # docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
@@ -105,12 +105,12 @@ process() {
                   notify "Lauching container"
                   docker_up "dev"
                   notify "Dumping database"
-                  docker_dump_sql "prod"
+                  docker_dump_sql "dev"
                   notify "Migrating django DB"
                   docker_migrate "dev"
                   notify "Indexing"
                 #  docker_index
-		              docker exec dev_django_web_01 bash -c "python manage.py index_pustakalaya --settings=pustakalaya.settings.production"
+		  docker exec dev_django_web_01 bash -c "python manage.py index_pustakalaya --settings=pustakalaya.settings.production"
                   get_deploy_version
 		              notify "Collecting statics"
                   docker_collectstatic "dev" | grep "static files copied"
