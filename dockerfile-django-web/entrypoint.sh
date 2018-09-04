2e50c7420224f88a6132908495bf6e71e6498dfc
Bikram Dhoju <bikram.dhoju@olenepal>
2018-09-04 11:19:10 +0545

    run and clean script added

--- dockerfile-django-web/entrypoint.sh
+++ dockerfile-django-web/entrypoint.sh
@@ -20,25 +20,25 @@ do
   sleep 10
 done
 
-if [ "DJANGO_DATABASE_HOST" = "pgmaster" ]; then
-    sleep 10 # wait for dockerized postgress will warm up
-fi
+# if [ "DJANGO_DATABASE_HOST" = "pgmaster" ]; then
+#     sleep 10 # wait for dockerized postgress will warm up
+# fi
 
 echo "$(date) - connected successfully"
 
 python manage.py migrate --settings=pustakalaya.settings.production >> /dev/stdout 2>&1 &&
 
-python manage.py index_pustakalaya --settings=pustakalaya.settings.production >> /dev/stdout 2>&1 &&
+
 
 python manage.py collectstatic --settings=pustakalaya.settings.production   --noinput >> /dev/stdout 2>&1
 
 # django-admin migrate --noinput
 # django-admin collectstatic --noinput
 
-# for PyCharm remote python interpretator
-if ! [ -z "${DJANGO_DEV}" ]; then
-    /usr/sbin/sshd
-fi
+# # for PyCharm remote python interpretator
+# if ! [ -z "${DJANGO_DEV}" ]; then
+#     /usr/sbin/sshd
+# fi
 
 if ! [ -z "${DJANGO_DEBUG}" ]; then
     django-admin runserver 0.0.0.0:8001
@@ -50,5 +50,6 @@ else
         --user=$APP_USER  \
          -b 0.0.0.0:8001 \
          --timeout=90 \
-         --log-file=/var/log/gunicorn-error.log
+         --log-file=/var/log/gunicorn-error.log &&
+    python manage.py index_pustakalaya --settings=pustakalaya.settings.production >> /dev/stdout 2>&1 
 fi
