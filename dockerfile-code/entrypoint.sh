752f521652484aecd6b740885ac9bf616840cb8b
bikram.dhoju <bikram.dhoju@olenepal.org>
2018-09-07 14:03:07 +0545

    version s1.2

--- dockerfile-code/entrypoint.sh
+++ dockerfile-code/entrypoint.sh
@@ -46,11 +46,11 @@ if ! [ -z "${DJANGO_DEBUG}" ]; then
     django-admin runserver 0.0.0.0:8001
 else
     export DJANGO_SETTINGS_MODULE=pustakalaya.settings.production
-    exec gunicorn ${DJANGO_WSGI_MODULE}:application \
-        --name $GUNICORN_NAME \
-        --workers $GUNICORN_NUM_WORKERS \
-        --user=$APP_USER  \
-         -b 0.0.0.0:8001 \
-         --timeout=90 \
-         --log-file=/var/log/gunicorn-error.log 
+    gunicorn ${DJANGO_WSGI_MODULE}:application \
+    --name $GUNICORN_NAME \
+    --workers $GUNICORN_NUM_WORKERS \
+    --user=$APP_USER  \
+    -b 0.0.0.0:8001 \
+    --timeout=90 \
+    --log-file=/var/log/gunicorn-error.log 
 fi
