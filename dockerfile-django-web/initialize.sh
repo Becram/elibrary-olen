957abee35c488b338a8102979d3b73f3f95b8dd4
Bikram Dhoju <bikram.dhoju@olenepal>
2018-10-03 13:35:40 +0545

    depoyment cleaned

--- dockerfile-django-web/initialize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-python manage.py migrate --settings=pustakalaya.settings.production >> /dev/stdout 2>&1 &&
-
-python manage.py index_pustakalaya --settings=pustakalaya.settings.production >> /dev/stdout 2>&1 &&
-
-python manage.py collectstatic --settings=pustakalaya.settings.production   --noinput >> /dev/stdout 2>&1
