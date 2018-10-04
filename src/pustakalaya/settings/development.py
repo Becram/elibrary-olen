d1e9dc20d0d2d564bead9ca8bab1b1804383f8e4
Bikram Dhoju <bikram.dhoju@olenepal>
2018-10-04 10:27:30 +0545

    test

--- src/pustakalaya/settings/development.py
+++ src/pustakalaya/settings/development.py
@@ -1,14 +1,20 @@
-from .base import * # NOQA
+from .base import *  # NOQA
 
-INSTALLED_APPS += [
-    "debug_toolbar",
-]
+DEBUG = False
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+#100MB
+FILE_UPLOAD_MAX_MEMORY_SIZE=104857600
 
+#CELERY_BROKER_URL = 'amqp://rabbitmq'
 
+try:
+    db_name = config["DATABASE"]["NAME"]
+    db_user = config["DATABASE"]["USER"]
+    db_password = config["DATABASE"]["PASSWORD"]
+    db_host = config["DATABASE"]["HOST"]
+    db_port = config["DATABASE"]["PORT"]
+except KeyError:
+    raise ImproperlyConfigured("Improperly configured database settings.")
 
 DATABASES = {
     'default': {
@@ -26,4 +32,3 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
